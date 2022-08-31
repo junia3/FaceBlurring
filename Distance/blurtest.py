8d9d4229d5c6fc4b7372508d4d688fe3c169a181
Junyoung Park <79881119+junia3@users.noreply.github.com>
2022-09-01 08:21:10 +0900

    Update blurtest.py

--- Distance/blurtest.py
+++ Distance/blurtest.py
@@ -35,4 +35,4 @@ def blurring(img, d=None, angle=None):
         cv2.normalize(blurred, blurred, 0, 255, cv2.NORM_MINMAX)
         blurred = np.array(blurred, dtype=np.uint8)
 
-    return blurred, random_degree/100
+    return blurred
