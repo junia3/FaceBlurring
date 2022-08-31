ef0f781dd5e278f4052930db4ffd37cab627dc97
Junyoung Park <79881119+junia3@users.noreply.github.com>
2022-09-01 08:22:07 +0900

    Update sampling.py

--- Distance/sampling.py
+++ Distance/sampling.py
@@ -15,7 +15,7 @@ for file in os.listdir(image_root):
     clean_img = cv2.imread(os.path.join(image_root, file))
     print(f"Creating blur images [{look_upto}/30]")
     for i in tqdm(range(101)):
-        blurred_img, _ = blurring(clean_img, d=i, angle=45)
+        blurred_img = blurring(clean_img, d=i, angle=45)
         cv2.imwrite(os.path.join(image_root, image_name, f'fix_{i}.png'), blurred_img)
 
     if look_upto == 30:
